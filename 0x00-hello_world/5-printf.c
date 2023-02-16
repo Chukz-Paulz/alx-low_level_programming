28a010aec38d5266ae44b3f6710bdb16379173e6
Nnadi Chukwuka <chukwukannadi@yahoo.com>
2023-02-16 15:48:56 -0800

    5-printf.c commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
- * main- prints a string followed by a new line
- * Return: 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
