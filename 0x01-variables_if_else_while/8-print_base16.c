65bff07534baf433d59c9b11e8c54e195964b76a
Nnadi Chukwuka <chukwukannadi@yahoo.com>
2023-02-17 16:49:59 -0800

    8-print_base16.c commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+int main(void)
+/**
+ * main - Entry point
+ *
+ * Description: Prints all the numbers of base 16 in lowercase
+ *              followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+{
+int num = 0;
+char letter = 'a';
+while (num < 10)
+{
+putchar(num + '0');
+num++;
+}
+while (letter < 'g')
+{
+putchar(letter);
+letter++;
+}
+putchar('\n');
+return (0);
+}
