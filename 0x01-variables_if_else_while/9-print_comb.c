8610a71b252d5e5204c4cf73bb68517285ccf909
Nnadi Chukwuka <chukwukannadi@yahoo.com>
2023-02-17 18:04:51 -0800

    9-print_comb.c commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,29 +2,21 @@
 /**
  * main - Entry point
  *
- * Prints all possible combinations of single-digit numbers.
- * Separates Numbers by commas and spaces and printed in ascending order.
- * Uses putchar function exclusively, with a maximum of four putchar calls.
- * No variables of type char are used.
+ * Description: Prints all possible combinations of single-digit numbers
+ * separated by commas and spaces, and printed in ascending order.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int num1, num2;
-for (num1 = 0; num1 < 9; num1++)
+int num;
+for (num = 0; num <= 9; num++)
 {
-for (num2 = num1 + 1; num2 <= 9; num2++)
-{
-putchar((num1 % 10) + '0');
-putchar(',');
-putchar(' ');
-putchar((num2 % 10) + '0');
-if (num1 != 8 || num2 != 9)
+putchar(num + '0');
+if (num < 9)
 {
 putchar(',');
 putchar(' ');
-}
 }
 }
 putchar('\n');
