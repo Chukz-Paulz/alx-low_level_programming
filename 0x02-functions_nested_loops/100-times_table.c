eab3a484f76c4522ea2fdb9be8897042541d95d9
Nnadi Chukwuka <chukwukannadi@yahoo.com>
2023-02-21 19:38:12 -0800

    100-time_table.c commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,25 +9,21 @@
  */
 void print_times_table(int n)
 {
-int i, j, product;
+if (n > 15 || n < 0)
+return;
 
-if (n >= 0 && n <= 15)
-{
-for (i = 0; i <= n; i++)
-{
-for (j = 0; j <= n; j++)
-{
-product = i * j;
-if (j == 0)
-printf("%d", product);
-else if (product < 10)
-printf("   %d", product);
-else if (product < 100)
+for (int i = 0; i <= n; i++) {
+for (int j = 0; j <= n; j++) {
+int product = i * j;
+if (j != 0)
+printf(", ");
+if (product < 10)
 printf("  %d", product);
-else
+else if (product < 100)
 printf(" %d", product);
+else
+printf("%d", product);
 }
 printf("\n");
-}
 }
 }
