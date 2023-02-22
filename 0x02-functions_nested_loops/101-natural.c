3119180fdd125be530ff2d676ae26b071df2ad38
Nnadi Chukwuka <chukwukannadi@yahoo.com>
2023-02-21 20:23:07 -0800

    101-natural.c commit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,8 +3,10 @@
 int main(void) {
 int i, sum = 0;
 
-for (i = 0; i < 1024; i++) {
-if (i % 3 == 0 || i % 5 == 0) {
+for (i = 0; i < 1024; i++) 
+{
+if (i % 3 == 0 || i % 5 == 0) 
+{
 sum += i;
 }
 }
