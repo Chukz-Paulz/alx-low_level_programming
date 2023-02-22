d85e948ca3257465f58485d3b4781e7e1079eadd
Nnadi Chukwuka <chukwukannadi@yahoo.com>
2023-02-21 18:43:37 -0800

    10-add.c commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Sum of the two integers
+ */
+int add(int a, int b)
+{
+return a + b;
+}
