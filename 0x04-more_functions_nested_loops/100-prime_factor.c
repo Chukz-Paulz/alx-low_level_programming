4070f30da87450b0272d748db650deefd0feddd9
Nnadi Chukwuka <chukwukannadi@yahoo.com>
2023-02-23 18:40:32 -0800

    100-prime_factor.c commit

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -12,13 +12,15 @@ int main(void)
 long num = 612852475143;
 long i;
 
-for (i = 2; i <= sqrt(num); i++) {
+for (i = 2; i <= sqrt(num); i++) 
+{
 while (num % i == 0) {
 num /= i;
 }
 }
 
-if (num > 1) {
+if (num > 1) 
+{
 i = num;
 }
 
