4ecabe36a6bdab54cef1f5ab0663ee0ec82ccf16
Nnadi Chukwuka <chukwukannadi@yahoo.com>
2023-02-23 19:11:15 -0800

    100-prime_factor.c commit

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <math.h>
-
 /**
  * main - prints prime factors of 612852475143
  *
@@ -8,28 +6,5 @@
  */
 int main(void)
 {
-int num = 612852475143;
-int i;
-
-while (num % 2 == 0) 
-{
-num /= 2;
-}
-
-for (i = 3; i <= sqrt(num); i += 2) 
-{
-while (num % i == 0) 
-{
-num /= i;
-}
-}
-
-if (num > 1) 
-{
-i = num;
-}
-
-_putchar("%d\n", i);
-
-return (0);
+	return (0);
 }
