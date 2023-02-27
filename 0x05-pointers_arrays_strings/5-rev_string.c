e222292e522584b83d8c6d2971f215bcbd73fbeb
Nnadi Chukwuka <chukwukannadi@yahoo.com>
2023-02-27 05:47:42 -0800

    5-rev_string.c commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,10 +11,10 @@ void rev_string(char *s)
 	char tmp;
 
 	while (s[len] != '\0')
-		len++;
+	len++;
 
 	for (i = 0; i < len / 2; i++)
- 	{
+	{
 		tmp = s[i];
 		s[i] = s[len - i - 1];
 		s[len - i - 1] = tmp;
