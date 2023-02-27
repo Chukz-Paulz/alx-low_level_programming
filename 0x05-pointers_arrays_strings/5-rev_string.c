06bb89ae61f8e662ccc526c0821a38183832fd21
Nnadi Chukwuka <chukwukannadi@yahoo.com>
2023-02-27 05:31:39 -0800

    5-rev_string.c commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,33 +1,24 @@
 #include "main.h"
 
 /**
-  * rev_string - Reverses a string
-  * @s: The string to be modified
-  *
-  * Return: void
-  */
+ * rev_string - reverses a string
+ * @s: the string to reverse
+ */
 void rev_string(char *s)
 {
-	int i, c, k;
-	char *a, aux;
+	int len = 0, i;
+	char tmp;
 
-	a = s;
+	/* find the length of the string */
+	while (s[len] != '\0')
+		len++;
 
-	while (s[c] != '\0')
-	{
-		c++;
-	}
-
-	for (k = 1; k < c; k++)
-	{
-		a++;
-	}
-
-	for (i = 0; i < (c / 2); i++)
-	{
-		aux = s[i];
-		s[i] = *a;
-		*a = aux;
-		a--;
+	/* swap characters from both ends of the string */
+	for (i = 0; i < len / 2; i++)
+ 	{
+		tmp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = tmp;
 	}
 }
+
