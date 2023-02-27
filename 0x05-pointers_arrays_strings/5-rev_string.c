2ddb12c2b7e65618772d236ae26cefea42904a51
Nnadi Chukwuka <chukwukannadi@yahoo.com>
2023-02-27 05:34:38 -0800

    5-rev_string.c commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,17 +3,16 @@
 /**
  * rev_string - reverses a string
  * @s: the string to reverse
+ * return: void
  */
 void rev_string(char *s)
 {
 	int len = 0, i;
 	char tmp;
 
-	/* find the length of the string */
 	while (s[len] != '\0')
 		len++;
 
-	/* swap characters from both ends of the string */
 	for (i = 0; i < len / 2; i++)
  	{
 		tmp = s[i];
