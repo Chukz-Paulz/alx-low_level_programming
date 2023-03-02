ff67f68a2b195faeb95262aaebc20fee8789dded
Nnadi Chukwuka <chukwukannadi@yahoo.com>
2023-03-01 18:17:07 -0800

    7-leet.c commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,27 +2,28 @@
 
 /**
  * leet - encode a string into 1337
- * @s: string
- * Return: encoded string `s`
+ * @s: string to be encoded
+ *
+ * Return: pointer to encoded string
  */
-
 char *leet(char *s)
 {
-	int i;
+	int i, j;
+	char letter[] = "aeotlAEOTL";
+	char number[] = "4307104310";
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		while (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		while (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		while (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		while (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		while (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (i = 0; s[i] != '\0'; i++)
+		{		
+			for (j = 0; letter[j] != '\0'; j++)
+			{
+				if (s[i] == letter[j])
+				{
+					s[i] = number[j];
+					break;
+			}
+		}
 	}
 
-	return (s);
+return (s);
 }
+
