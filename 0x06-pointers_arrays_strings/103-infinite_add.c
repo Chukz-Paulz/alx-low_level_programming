f7a52131cf249ba7256c79b49036d3ab581b2a67
Nnadi Chukwuka <chukwukannadi@yahoo.com>
2023-03-02 03:15:15 -0800

    103-infinite_add.c commit

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,82 +1,51 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * infinite_add - Adds two numbers
- * @n1: First number
- * @n2: Second number
- * @r: Buffer to store result
- * @size_r: Size of buffer
+ * infinite_add - Adds two numbers.
+ * @n1: The first number to be added.
+ * @n2: The second number to be added.
+ * @r: Pointer to the buffer where the result will be stored.
+ * @size_r: The size of the buffer.
  *
- * Return: Pointer to result or 0 if buffer is too small
+ * Return: A pointer to the result, or 0 if the result can't be stored.
  */
-
-/* Add two numbers represented as strings */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int i = 0, j = 0, k = 0, carry = 0, sum = 0;
-int n1_len = 0, n2_len = 0, max_len = 0;
+int i, j, k, l1, l2, sum, carry = 0;
 
-/* Calculate length of n1 */
-while (n1[n1_len] != '\0')
-{
-n1_len++;
-}
-
-/* Calculate length of n2 */
-while (n2[n2_len] != '\0')
-{
-n2_len++;
-}
-
-/* Calculate maximum length */
-max_len = (n1_len > n2_len) ? n1_len : n2_len;
-
-/* Check if result can fit in buffer */
-if (max_len + 1 > size_r)
-{
-return 0;
-}
-
-/* Add numbers from right to left */
-for (i = n1_len - 1, j = n2_len - 1, k = max_len; k >= 0; i--, j--, k--)
+for (l1 = 0; n1[l1]; l1++)
+	;
+for (l2 = 0; n2[l2]; l2++)
+	;
+if (l1 + 2 > size_r || l2 + 2 > size_r)
+return (0);
+i = l1 - 1, j = l2 - 1, k = 0;
+while (i >= 0 || j >= 0 || carry)
 {
 sum = carry;
-
 if (i >= 0)
-{
-sum += n1[i] - '0';
-}
+sum += n1[i--] - '0';
 if (j >= 0)
-{
-sum += n2[j] - '0';
-}
-
+sum += n2[j--] - '0';
+if (k + 1 < size_r)
+r[k++] = sum % 10 + '0';
 carry = sum / 10;
-r[k] = (sum % 10) + '0';
 }
-
-/* Add carry if necessary */
-if (carry != 0) {
-if (max_len + 2 > size_r)
+if (k == 0)
+{
+r[k++] = '0';
+}
+if (k + 1 > size_r)
 {
 return (0);
 }
-
-for (i = max_len + 1; i >= 1; i--)
+r[k] = '\0';
+for (i = 0, j = k - 1; i < j; i++, j--)
 {
-r[i] = r[i - 1];
+char c = r[i];
+r[i] = r[j];
+r[j] = c;
 }
-
-r[0] = carry + '0';
-}
-
-/* Add terminating null character */
-if (max_len + 1 <= size_r)
-{
-r[max_len + 1] = '\0';
-}
-
 return (r);
 }
 
