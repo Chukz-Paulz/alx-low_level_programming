889484f3f9f6fb786a09369b5bbf469c51b2399f
Nnadi Chukwuka <chukwukannadi@yahoo.com>
2023-03-01 20:56:36 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,52 +1,56 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
 
 /**
  * infinite_add - adds two numbers
- * @n1: first number to add
- * @n2: second number to add
- * @r: buffer to store result
- * @size_r: size of buffer
+ * @n1: the first number
+ * @n2: the second number
+ * @r: the buffer to store the result
+ * @size_r: the size of the buffer
  *
- * Return: pointer to result, or 0 if result can't be stored in buffer
+ * Return: a pointer to the result or 0 if the result can't be stored in r
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, l1, l2, sum, carry;
+    int i, j, k, carry, len1, len2;
 
-	/* find lengths of n1 and n2 */
-	for (l1 = 0; n1[l1]; l1++)
-		;
-	for (l2 = 0; n2[l2]; l2++)
-		;
+    len1 = strlen(n1);
+    len2 = strlen(n2);
 
-	if (l1 > size_r || l2 > size_r)
-		return (0);
+    if (len1 > size_r || len2 > size_r)
+        return 0;
 
-	i = l1 - 1;
-	j = l2 - 1;
-	k = 0;
-	carry = 0;
+    i = len1 - 1;
+    j = len2 - 1;
+    k = 0;
+    carry = 0;
 
-	/* add digits from n1 and n2, and store result in buffer */
-	while (i >= 0 || j >= 0 || carry > 0)
-	{
-		sum = carry;
-		if (i >= 0)
-			sum += n1[i--] - '0';
-		if (j >= 0)
-			sum += n2[j--] - '0';
-		r[k++] = (sum % 10) + '0';
-		carry = sum / 10;
-	}
-	r[k] = '\0';
+    while (i >= 0 || j >= 0 || carry)
+    {
+        int digit1 = i >= 0 ? n1[i] - '0' : 0;
+        int digit2 = j >= 0 ? n2[j] - '0' : 0;
 
-	/* reverse the result */
-	for (i = 0, j = k - 1; i < j; i++, j--)
-	{
-		char temp = r[i];
-		r[i] = r[j];
-		r[j] = temp;
-	}
+        int sum = digit1 + digit2 + carry;
 
-	return (r);
+        if (k >= size_r)
+            return 0;
+
+        r[k++] = sum % 10 + '0';
+        carry = sum / 10;
+
+        i--;
+        j--;
+    }
+
+    r[k] = '\0';
+
+    for (i = 0, j = k - 1; i < j; i++, j--)
+    {
+        char tmp = r[i];
+        r[i] = r[j];
+        r[j] = tmp;
+    }
+
+    return r;
 }
