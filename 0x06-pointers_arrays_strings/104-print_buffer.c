7eee2f09fd8fc686087acf5bc0af5ea10aad0b0b
Nnadi Chukwuka <chukwukannadi@yahoo.com>
2023-03-01 20:32:22 -0800

    104-print_buffer.c commit

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,50 +1,51 @@
 #include <stdio.h>
 #include "main.h"
+
 void print_buffer(char *b, int size)
 {
-        int i, j;
+	int i, j;
 
-        if (size <= 0) {
-        printf("\n");
-        return;
-        }
+	if (size <= 0) {
+	printf("\n");
+	return;
+	}
 
-                        for (i = 0; i < size; i += 10) {
-                        printf("%08x: ", i);
-                        for (j = 0; j < 10; j++) {
-                if (i + j < size)
-                {
-                        printf("%02x", (unsigned char)b[i+j]);
-                }
-                        else
-                {
-                        printf("  ");
-                }
-                if (j % 2 == 1)
-                {
-                        printf(" ");
-                }
-                        }
-                        for (j = 0; j < 10; j++)
-                        {
-                if (i + j < size)
-                {
-                char c = b[i+j];
-                        if (c >= ' ' && c <= '~')
-                {
-                        printf("%c", c);
-                }
-                        else
-                        {
-                        printf(".");
-                }
-                }
-                        else
-                        {
-                        break;
-                        }
-                }
-                printf("\n");
-        }
-}
-
+		for (i = 0; i < size; i += 10) {
+			printf("%08lx: ", (unsigned long int)i);
+		for (j = 0; j < 10; j++) 
+		{
+		if (i + j < size) 
+		{
+			printf("%02x", (unsigned char)b[i+j]);
+		} 
+		else 
+		{
+			printf("  ");
+		}
+		if (j % 2 == 1);
+	}
+			printf(" ");
+	}
+	}
+		for (j = 0; j < 10; j++) 
+{
+			if (i + j < size) 
+			{
+		char c = b[i+j];
+			if (c >= ' ' && c <= '~') 
+		{
+			printf("%c", c);
+                } 
+		else 
+		{
+			printf(".");
+		}
+	} 
+		else 
+		{
+			break;
+		}
+		}
+		printf("\n");
+	}
+}    
