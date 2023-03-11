4a33c11d442f7cd8de430ddab83c92c2206f3bc7
Nnadi Chukwuka <chukwukannadi@yahoo.com>
2023-03-11 05:19:26 -0800

    0x09-static_libraries commit

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,14 +1,18 @@
 #include "main.h"
-void _puts(char *str)
-#include<stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+#include <stdio.h>
 
+/**
+ * _puts - prints a string to stdout
+ * @str: the string to print
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i++;
+	}
+	putchar('\n');
+}
