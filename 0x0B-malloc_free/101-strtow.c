a7cc5c9c80ae03146c731bea80b9263e3b5f6f1f
Nnadi Chukwuka <chukwukannadi@yahoo.com>
2023-03-13 18:19:44 -0700

    101-strtow.c commit

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -18,31 +18,8 @@ void print_tab(char **tab)
 			_putchar(tab[i][j]);
 		}
 		_putchar('\n');
-	}
-}
 
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
-{
-	char **tab;
-
-	tab = strtow("      ALX School         #cisfun      ");
-	if (tab == NULL)
-	{
-		_putchar('F');
-		_putchar('a');
-		_putchar('i');
-		_putchar('l');
-		_putchar('e');
-		_putchar('d');
-		_putchar('\n');
-		return (1);
 	}
-	print_tab(tab);
-	return (0);
+	return (1);
 }
 
