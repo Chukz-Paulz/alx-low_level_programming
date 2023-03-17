2858edff7d67f63a74f64f2bde50490812b1155d
Nnadi Chukwuka <chukwukannadi@yahoo.com>
2023-03-16 20:07:55 -0700

    101-mul.c commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,106 +1,75 @@
 #include <string.h>
 #include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-/**
- * _isdigit - checks if a character is a digit
- * @c: the character to check
- *
- * Return: 1 if c is a digit, 0 otherwise
- */
-int _isdigit(char c)
+
+int is_digit(char *s)
 {
-	return (c >= '0' && c <= '9');
+    while (*s) {
+        if (*s < '0' || *s > '9') {
+            return (0);
+        }
+        s++;
+    }
+    return (1);
 }
 
-/**
- * mul - multiplies two positive numbers
- * @num1: the first number
- * @num2: the second number
- *
- * Return: a pointer to the result, or NULL if an error occurred
- */
-char *mul(char *num1, char *num2)
+void print_error(void)
 {
-	int len1, len2, i, j, carry, n1, n2, res;
-	char *result;
-
-	len1 = strlen(num1);
-	len2 = strlen(num2);
-	result = calloc(len1 + len2 + 1, sizeof(char));
-	if (result == NULL)
-		return (NULL);
-	for (i = len1 - 1; i >= 0; i--)
-	{
-		if (!_isdigit(num1[i]))
-		{
-			free(result);
-			return (NULL);
-		}
-		carry = 0;
-		n1 = num1[i] - '0';
-		for (j = len2 - 1; j >= 0; j--)
-		{
-			if (!_isdigit(num2[j]))
-			{
-				free(result);
-				return (NULL);
-			}
-			n2 = num2[j] - '0';
-			res = n1 * n2 + carry + result[i + j + 1];
-			result[i + j + 1] = res % 10;
-			carry = res / 10;
-		}
-		result[i + j + 1] = carry;
-	}
-	return (result);
+    printf("Error\n");
+    exit(98);
 }
 
-/**
- * main - multiplies two positive numbers
- * @argc: the number of arguments
- * @argv: the argument vector
- *
- * Return: 0 on success, 98 on error
- */
 int main(int argc, char **argv)
 {
-	int i, j;
-	char *result;
+    int i, j, k, carry, len1, len2;
+    int *result;
 
-	if (argc != 3)
-	{
-		printf("Error\n");
-		return (98);
-	}
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j]; j++)
-		{
-			if (!_isdigit(argv[i][j]))
-			{
-				printf("Error\n");
-				return (98);
-			}
-		}
-	}
-	result = mul(argv[1], argv[2]);
-	if (result == NULL)
-	{
-		printf("Error\n");
-		return (98);
-	}
-	for (i = 0; result[i] == 0; i++)
-		;
-	if (result[i] == '\0')
-		printf("0\n");
-	else
-	{
-		for (; result[i]; i++)
-			putchar(result[i] + '0');
-		putchar('\n');
-	}
-	free(result);
-	return (0);
+    if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2])) {
+        print_error();
+    }
+
+    len1 = 0;
+    while (argv[1][len1]) {
+        len1++;
+    }
+
+    len2 = 0;
+    while (argv[2][len2]) {
+        len2++;
+    }
+
+    result = malloc((len1 + len2) * sizeof(int));
+    if (!result) {
+        return (1);
+    }
+
+    for (i = 0; i < len1 + len2; i++) {
+        result[i] = 0;
+    }
+
+    for (i = len1 - 1; i >= 0; i--) {
+        carry = 0;
+        for (j = len2 - 1; j >= 0; j--) {
+            k = i + j + 1;
+            carry += (argv[1][i] - '0') * (argv[2][j] - '0') + result[k];
+            result[k] = carry % 10;
+            carry /= 10;
+        }
+        if (carry) {
+            result[i + j + 1] += carry;
+        }
+    }
+
+    i = 0;
+    while (result[i] == 0 && i < len1 + len2 - 1) {
+        i++;
+    }
+    for (; i < len1 + len2; i++) {
+        _putchar(result[i] + '0');
+    }
+    _putchar('\n');
+    free(result);
+    return (0);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,5 +8,7 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char *mul(char *num1, char *num2);
+void print_error(void);
+int main(int argc, char **argv);
 
 #endif
