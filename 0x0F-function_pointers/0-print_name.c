9e4f9edf78d94cc366be353c1aa08c34e2f5b809
Nnadi Chukwuka <chukwukannadi@yahoo.com>
2023-03-22 19:14:03 -0700

    0-print_name.c commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,13 @@
 #include "function_pointers.h"
-#include <stdio.h>
 /**
- * print_name - print name using pointer to function
- * @name: string to add
- * @f: pointer to function
+ * print_name - prints a name
+ * @name: the name to print
+ * @f: the function to send the name to
  * Return: nothing
- **/
+ */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
+	if (!name || !f)
 		return;
 	f(name);
 }
