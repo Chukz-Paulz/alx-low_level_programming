3b0e3f554307f37fc1337aed26cc0f155bf57382
Nnadi Chukwuka <chukwukannadi@yahoo.com>
2023-04-03 16:16:26 -0700

    102-free_listint_safe.c commit

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,93 +1,36 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
- * looped_listint_len - Counts the number of unique nodes
- * in a looped listint_t linked list.
- * @head: A pointer to the head of the listint_t to check.
+ * free_listint_safe - frees a linked list
+ * @h: pointer to the first node in the linked list
  *
- * Return: If the list is not looped - 0.
- * Otherwise - the number of unique nodes in the list.
- */
-size_t looped_listint_len(const listint_t *head)
-{
-    const listint_t *tortoise, *hare;
-    size_t nodes = 1;
-
-    if (head == NULL || head->next == NULL)
-        return (0);
-
-    tortoise = head->next;
-    hare = (head->next)->next;
-
-    while (hare)
-    {
-        if (tortoise == hare)
-        {
-            tortoise = head;
-
-            while (tortoise != hare)
-            {
-                nodes++;
-                tortoise = tortoise->next;
-                hare = hare->next;
-            }
-
-            tortoise = tortoise->next;
-
-            while (tortoise != hare)
-            {
-                nodes++;
-                tortoise = tortoise->next;
-            }
-
-            return (nodes);
-        }
-
-        tortoise = tortoise->next;
-        hare = (hare->next)->next;
-    }
-
-    return (0);
-}
-
-/**
- * free_listint_safe - Frees a listint_t list safely.
- * @h: A pointer to the head of the listint_t list.
- *
- * Return: The size of the list that was free'd.
+ * Return: number of elements in the freed list
  */
 size_t free_listint_safe(listint_t **h)
 {
-    size_t nodes, index = 0;
-    listint_t *current, *tmp;
-
-    nodes = looped_listint_len(*h);
-
-    if (nodes == 0)
-    {
-        while (*h != NULL)
-        {
-            current = (*h);
-            *h = (*h)->next;
-            free(current);
-            index++;
-        }
-    }
-    else
-    {
-        for (index = 0; index < nodes; index++)
-        {
-            printf("[%p] %d\n", (void *)*h, (*h)->n);
-            current = (*h);
-            *h = (*h)->next;
-            free(current);
-        }
-        printf("-> [%p] %d\n", (void *)*h, (*h)->n);
-        *h = NULL;
-        nodes++;
-    }
-
-	return (nodes);
+ size_t len = 0;
+ int diff;
+ listint_t *temp;
+ if (!h || !*h)
+ return (0);
+ while (*h)
+ {
+ diff = *h - (*h)->next;
+ if (diff > 0)
+ {
+ temp = (*h)->next;
+ free(*h);
+ *h = temp;
+ len++;
+ }
+ else
+ {
+ free(*h);
+ *h = NULL;
+ len++;
+ break;
+ }
+ }
+ *h = NULL;
+ return (len);
 }
+
