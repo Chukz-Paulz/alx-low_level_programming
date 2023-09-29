b0b7461e1672a710cd9e5e23d0b3f5352d74c211
Chukz-Paulz <chukwukannadi@yahoo.com>
2023-09-28 19:01:33 -0700

    1-print_binary.c task

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+/**
+ * print_binary - prints decimal as binary
+ * @n: long integer
+ */
+
+void print_binary(unsigned long int n)
+{
+	/*declaration*/
+	signed long int size;
+	char z;
+	int flag;
+
+	size = sizeof(n) * 8 - 1;
+
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+
+	if (n == 1)
+	{
+		printf("1");
+		return;
+	}
+
+	flag = 0;
+
+	while (size >= 0)
+	{
+		z = (n >> size) & 1;
+
+		if (flag == 1)
+			_putchar(z + '0');
+		else
+		{
+			if (z == 1)
+			{
+				_putchar(z + '0');
+				flag = 1;
+			}
+		}
+
+		size -= 1;
+	}
+}
