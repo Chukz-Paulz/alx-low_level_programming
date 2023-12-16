bc45ff5dddf0bc8b8b254d99bd2290318037075e
Nnadi Chukwuka <chukwukannadi@yahoo.com>
2023-12-15 20:15:56 -0800

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -p
+wget -p .. https://raw.githubusercontent.com/Chukz-Paulz/alx-low_level_programming/master/0x18-dynamic_libraries/libjack_hack.so
+export LD_PRELOAD="$PWD/../libjack_hack.so"
